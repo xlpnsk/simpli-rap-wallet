dc990582856bfa7b4da51b87c9c0de28dc368315
shogun-pb <patrykbak1999@gmail.com>
2023-01-23 17:20:05 +0100

    New styleshet for Loing and Account

--- src/auth/Login.tsx
+++ src/auth/Login.tsx
@@ -1,8 +1,9 @@
 import * as React from "react";
-import { Alert, StyleSheet, View } from "react-native";
+import { Alert, StyleSheet, View, Text } from "react-native";
 import { supabase } from "../supabase";
 import { Input } from "react-native-elements";
 import { Button } from "../components/Button";
+import { Palette } from "../../style/palette";
 
 const Login: React.FC = () => {
   const [email, setEmail] = React.useState("");
@@ -33,20 +34,24 @@ const Login: React.FC = () => {
 
   return (
     <View>
+      <View style={[styles.verticallySpaced]}>
+        <Text style={[styles.text]}>Email</Text></View>
       <View style={[styles.verticallySpaced, styles.mt20]}>
-        <Input
-          label="Email"
-          leftIcon={{ type: "font-awesome", name: "envelope" }}
+        <Input 
+          style={[styles.input]}
+          leftIcon={{ type: "font-awesome", name: "envelope", color: "white" }}
           onChangeText={(text) => setEmail(text)}
           value={email}
           placeholder="email@address.com"
           autoCapitalize={"none"}
         />
       </View>
-      <View style={styles.verticallySpaced}>
+      <View style={[styles.verticallySpaced]}>
+        <Text style={[styles.text]}>Password</Text></View>
+      <View style={[styles.verticallySpaced, styles.mt20]}>
         <Input
-          label="Password"
-          leftIcon={{ type: "font-awesome", name: "lock" }}
+          style={[styles.input]}
+          leftIcon={{ type: "font-awesome", name: "lock", color: "white"}}
           onChangeText={(text) => setPassword(text)}
           value={password}
           secureTextEntry={true}
@@ -54,12 +59,12 @@ const Login: React.FC = () => {
           autoCapitalize={"none"}
         />
       </View>
-      <View style={[styles.verticallySpaced, styles.mt20]}>
+      <View style={[styles.verticallySpaced,styles.button ]}>
         <Button disabled={loading} onPress={() => signInWithEmail()}>
           Sign in
         </Button>
       </View>
-      <View style={styles.verticallySpaced}>
+      <View style={[styles.verticallySpaced,styles.button ]}>
         <Button disabled={loading} onPress={() => signUpWithEmail()}>
           Sign up
         </Button>
@@ -79,7 +84,36 @@ const styles = StyleSheet.create({
     alignSelf: "stretch",
   },
   mt20: {
-    marginTop: 20,
+    marginTop:5,
+    marginLeft:5,
+    marginRight:5,
+    borderRadius: 25,
+    backgroundColor: Palette.DarkBlue,
+    
+  },
+  button:{
+    marginLeft:5,
+    marginRight:5,
+  },
+  input:{
+    color: "#f8f8ff",
+    textDecorationColor: "#f8f8ff",
+    tintColor: "#f8f8ff",
+    baseColor: "white",
+    backgroundColor: Palette.DarkBlue,
+    borderRadius: 15,
+    padding: 10,
+  },
+  text:{
+    backgroundColor: Palette.Fuchsia,
+    borderRadius: 20,
+    padding: 10,
+    paddingBottom:20,
+    color: "white",
+    marginRight: 220,
+    marginLeft:20,
+    marginBottom: -25,
+    textAlign: "center",
   },
 });
 
